439e500b8790aabac7c3dcdf272dfe7c830695a2
Vijayan T <69061573+vijayancpt@users.noreply.github.com>
2022-10-25 17:23:23 +0530

    Updated path

--- src/App.tsx
+++ src/App.tsx
@@ -3,33 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 // import './firebase';
 
-import {getVegList,addValue,deleteData,updateData} from  './firebase';
+import { getVegList, addValue, deleteData, updateData } from './firebase';
 
 function App() {
 
-  const [vegList,setVegList] = useState('');
+  const [vegList, setVegList] = useState<any>([]);
 
-useEffect(()=>{
-  getVegList().then((result)=>{ 
-  console.log('VegList::',result)
-setVegList(JSON.stringify(result));
-});
+  useEffect(() => {
+    getVegList().then((result) => {
+      console.log('VegList::', result)
+      setVegList(result);
+    });
 
-},[]);
+  }, []);
 
   return (
-    <div className="App"  style={{backgroundColor:'red'}}>
+    <div className="App"  style={{backgroundColor:'green', height:'500px'}}>
       <header>
         <h3>VegList:</h3>
-        {
-          vegList
-        }
+       
+          
+          <table style={{
+            margin:'auto'
+          }} border={1}>
+            <th>Veg Name</th> <th>Veg Count</th>
+
+            {
+              vegList.map((curRow:any)=>(
+                <tr>
+                <td>{curRow.VegName}</td>
+                <td>{curRow.VegQuantity}</td>
+                </tr>
+              ))
+            }
+            
+          </table>
+
       </header>
+      <div style={{margin:'500px'}}>
       <button onClick={addValue}>Add Value</button>
       <button onClick={deleteData}>Delete Value</button>
       <button onClick={updateData}>Update Value</button>
+      </div>
 
-    </div>
+    </div >
 
   );
 }
