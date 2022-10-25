ba5c5a8edb259a4130aac77252ff67994c4bf702
Vijayan T <69061573+vijayancpt@users.noreply.github.com>
2022-10-25 14:57:33 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -19,19 +19,7 @@ setVegList(JSON.stringify(result));
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{backgroundColor:'red'}}>
         <h3>VegList:</h3>
         {
           vegList
