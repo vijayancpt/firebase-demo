7e718f02a6ad373a7c2e037a6ceb0aebf6cf5d61
Vijayan T <69061573+vijayancpt@users.noreply.github.com>
2024-03-27 14:25:18 +0530

    changed test cases

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App/>);
-  const linkElement = screen.getByText(/Veg Name/i);
+  const linkElement = screen.getByText(/Veg dName/i);
   expect(linkElement).toBeInTheDocument();
 });
